0c8de900b06334739ff1a43f5f1ffdab3cd72ff9
AsseliDaly <bentaherdaly123@gmail.com>
2025-08-26 18:37:48 +0100

    babayaga2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,14 +6,15 @@ import {Post} from './post/post';
 import {Home} from './home/home';
 import {Profile} from './profile/profile';
 import {Settings} from './settings/settings';
+import {authGuardGuard} from './guards/auth-guard-guard';
 
 export const routes: Routes = [
   {path:'',component:Home},
   {path:'feed',component:Feed},
   {path:'login',component:Login},
   {path:'register',component:Register},
-  {path:'post',component:Post},
-  { path: 'profile', component: Profile },
-  { path: 'settings', component: Settings },
+  {path:'post',component:Post ,canActivate: [authGuardGuard]},
+  { path: 'profile', component: Profile ,canActivate: [authGuardGuard]},
+  { path: 'settings', component: Settings ,canActivate: [authGuardGuard]},
 
 ];
--- src/app/feed/feed.ts
+++ src/app/feed/feed.ts
@@ -1,16 +1,13 @@
-import { Component } from '@angular/core';
-import {Header} from '../header/header';
-import {RouterOutlet} from '@angular/router';
-
+import {Component, ElementRef, ViewChild} from '@angular/core';
 @Component({
   selector: 'app-feed',
   imports: [
-    Header,
-    RouterOutlet
+
   ],
   templateUrl: './feed.html',
   styleUrl: './feed.css'
 })
 export class Feed {
-
+  videos: string[] = ['/video/Vidéo sans titre ‐ Réalisée avec Clipchamp (2).mp4','/video/reel insta.mp4'];
+  currentIndex :number = 0;
 }
